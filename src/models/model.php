d640228a4f6ae77bba5d6efff5a3f210231cf3a8
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-11-20 16:04:31 +0330

    Fix: something in model.

--- src/models/model.php
+++ src/models/model.php
@@ -4,17 +4,17 @@ abstract class BaseModel {
 	private string|null $error = null;
 
 	final public function hasError(): bool {
-		return BaseModel::$error !== null;
+		return $this->error !== null;
 	}
 
 	final public function getError(): string|null {
-		$error = BaseModel::$error;
-		BaseModel::$error = null;
+		$error = $this->error;
+		$this->error = null;
 		return $error;
 	}
 
 	final public function setError(string $err): void {
-		BaseModel::$error = $err;
+		$this->error = $err;
 	}
 
 	abstract public function validate(): bool;
