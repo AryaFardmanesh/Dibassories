13d71a3a1355d313f440ce0043fcd393e9fbfa62
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-11-24 12:16:44 +0330

    Add(repo): base method for create product.

--- src/repositories/products.php
+++ src/repositories/products.php
@@ -5,6 +5,19 @@ include_once __DIR__ . "/../models/products.php";
 include_once __DIR__ . "/../utils/uuid.php";
 include_once __DIR__ . "/../utils/sanitizer.php";
 
-class ProductRepository extends BaseRepository {}
+class ProductRepository extends BaseRepository {
+	public static function create(
+		string $owner,
+		int $type,
+		string $name,
+		string $description,
+		array $images,
+		int $count,
+		int $price,
+		int $offer
+	): void {
+		throw new \Exception("Not yet implemented.");
+	}
+}
 
 ?>
