8bc4373f5b76dd39346e336d003d16cc827a77d7
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-11-24 12:12:52 +0330

    Add(repo): base class for product repository.

---
+++ src/repositories/products.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once __DIR__ . "/repository.php";
+include_once __DIR__ . "/../models/products.php";
+include_once __DIR__ . "/../utils/uuid.php";
+include_once __DIR__ . "/../utils/sanitizer.php";
+
+class ProductRepository extends BaseRepository {}
+
+?>
