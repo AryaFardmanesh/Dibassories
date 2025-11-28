80c2e9d59f9c02b83793860720e08cf1ac93511d
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-11-28 14:28:28 +0330

    Add(repo): create method for order repository.

--- src/repositories/orders.php
+++ src/repositories/orders.php
@@ -5,9 +5,93 @@ include_once __DIR__ . "/repository.php";
 include_once __DIR__ . "/../models/orders.php";
 include_once __DIR__ . "/../utils/uuid.php";
 
-class CartSessionModelRepository extends BaseRepository {
-	final public static function create(): OrderModel|null {
-		throw new \Exception("Not yet implemented.");
+class OrderRepository extends BaseRepository {
+	final public static function create(
+		string $owner,
+		string $provider,
+		string $product,
+		string $productColor,
+		string $productMaterial,
+		string $productSize,
+		int $count,
+		int $total,
+		string $phone,
+		string $address,
+		string $zipcode
+	): OrderModel|null {
+		if (!OrderRepository::dbConnect()) {
+			OrderRepository::setError(Database::getError());
+			goto failed;
+		}
+
+		$model = new OrderModel(
+			uuid(),
+			$owner,
+			$provider,
+			$product,
+			$productColor,
+			$productMaterial,
+			$productSize,
+			$count,
+			$total,
+			$phone,
+			$address,
+			$zipcode
+		);
+
+		if ($model->hasError()) {
+			OrderRepository::setError($model->getError());
+			goto failed;
+		}
+
+		$id = $model->id;
+		$status = $model->status;
+
+		Database::query(
+			"INSERT INTO `dibas_orders` (
+				`id`,
+				`owner`,
+				`provider`,
+				`product`,
+				`product_color`,
+				`product_material`,
+				`product_size`,
+				`count`,
+				`total`,
+				`phone`,
+				`address`,
+				`zipcode`,
+				`status`,
+				`created_at`
+			) VALUES (
+				'$id',
+				'$owner',
+				'$provider',
+				'$product',
+				'$productColor',
+				'$productMaterial',
+				'$productSize',
+				$count,
+				$total,
+				'$phone',
+				'$address',
+				'$zipcode',
+				$status,
+				CURRENT_TIMESTAMP()
+			);"
+		);
+
+		if (Database::hasError()) {
+			OrderRepository::setError(Database::getError());
+			goto failed;
+		}
+
+		Database::close();
+		return $model;
+
+		failed:
+			Database::close();
+			return null;
 	}
 
 	final public static function remove(): OrderModel|null {
