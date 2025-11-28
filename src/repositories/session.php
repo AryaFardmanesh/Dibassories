e61bd7a2197e8a80d27bdb5c286e88b47f4625f8
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-11-28 13:53:48 +0330

    Add(repo): create method for session repository.

--- src/repositories/session.php
+++ src/repositories/session.php
@@ -2,12 +2,54 @@
 
 include_once __DIR__ . "/../config.php";
 include_once __DIR__ . "/repository.php";
-include_once __DIR__ . "/../models/session.php";
+include_once __DIR__ . "/../models/sessions.php";
 include_once __DIR__ . "/../utils/uuid.php";
 
 class CartSessionModelRepository extends BaseRepository {
 	final public static function create(string $owner): CartSessionModel|null {
-		throw new \Exception("Not implemented yet.");
+		if (!CartSessionModelRepository::dbConnect()) {
+			CartSessionModelRepository::setError(Database::getError());
+			goto failed;
+		}
+
+		$model = new CartSessionModel(
+			uuid(),
+			$owner
+		);
+
+		if ($model->hasError()) {
+			CartSessionModelRepository::setError($model->getError());
+			goto failed;
+		}
+
+		$id = $model->id;
+		$status = $model->status;
+
+		Database::query(
+			"INSERT INTO `dibas_cart_sessions` (
+				`id`,
+				`owner`,
+				`status`,
+				`created_at`
+			) VALUES (
+				'$id',
+				'$owner',
+				$status,
+				CURRENT_TIMESTAMP()
+			);"
+		);
+
+		if (Database::hasError()) {
+			CartSessionModelRepository::setError(Database::getError());
+			goto failed;
+		}
+
+		Database::close();
+		return $model;
+
+		failed:
+			Database::close();
+			return null;
 	}
 
 	final public static function remove(string $owner): bool {
