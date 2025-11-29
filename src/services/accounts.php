65fa0b5768f7c4a884e8ee46ed87c290cab2c67f
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-11-29 11:20:20 +0330

    Add(service): account service class.

---
+++ src/services/accounts.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once __DIR__ . "/service.php";
+
+class AccountService extends BaseService {
+	final public static function login(string $username, string $password): bool {
+		throw new \Exception("Not implemented yet.");
+	}
+
+	final public static function signup(
+		string $username,
+		string $password,
+		string $passwordConfirm,
+		string $email,
+		string $fname,
+		string $lname,
+		string $address,
+		string $zipcode
+	): bool {
+		throw new \Exception("Not implemented yet.");
+	}
+
+	final public static function sellerRequest(
+		string $id,
+		string $pangirno,
+		string $card_number,
+		string $card_terminal,
+		string|null $instagram,
+		string|null $telegram
+	): bool {
+		throw new \Exception("Not implemented yet.");
+	}
+}
+
+?>
