32ddc2f7e817aa71c9ac9994f5d484232b0dff67
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-11-29 11:45:57 +0330

    Add(services): is login method for account service.

--- src/services/accounts.php
+++ src/services/accounts.php
@@ -1,10 +1,33 @@
 <?php
 
 include_once __DIR__ . "/service.php";
+include_once __DIR__ . "/../repositories/accounts.php";
+include_once __DIR__ . "/../config.php";
+include_once __DIR__ . "/../utils/cookie.php";
+include_once __DIR__ . "/../utils/jwt.php";
 
 class AccountService extends BaseService {
-	final public static function isLogin(string $username, string $password): bool {
-		throw new \Exception("Not implemented yet.");
+	final public static function isLogin(): bool {
+		$token = Cookie::get(COOKIE_JWT_NAME);
+
+		if ($token === null) {
+			return false;
+		}
+
+		$data = JWT::decode($token)["body"];
+		$id = $data["id"];
+		$exp = strtotime($data["exp"]["date"]);
+		$now = strtotime("today midnight");
+
+		if ($now >= $exp) {
+			return false;
+		}
+
+		if (AccountRepository::findById($id) === null) {
+			return false;
+		}
+
+		return true;
 	}
 
 	final public static function login(string $username, string $password): bool {
