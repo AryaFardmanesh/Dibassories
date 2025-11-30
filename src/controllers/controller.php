9c176d185e046e774c87a245393f0fdeca78482f
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-11-30 17:20:56 +0330

    Add(controller): method for error handling.

--- src/controllers/controller.php
+++ src/controllers/controller.php
@@ -4,6 +4,22 @@ include_once __DIR__ . "/../config.php";
 include_once __DIR__ . "/../utils/sanitizer.php";
 
 class Controller {
+	private static string|null $error = null;
+
+	final static public function hasError(): bool {
+		return Controller::$error !== null;
+	}
+
+	final static public function getError(): string|null {
+		$error = Controller::$error;
+		Controller::$error = null;
+		return $error;
+	}
+
+	final static public function setError(string $err): void {
+		Controller::$error = $err;
+	}
+
 	final public static function getRequest(string $name): string|null {
 		if (isset($_GET[$name])) {
 			return testInput($_GET[$name]);
@@ -15,6 +31,15 @@ class Controller {
 	final public static function redirect(string|null $path): never {
 		if ($path === null) {
 			$path = CONTROLLER_REDIRECT_URL;
+		}
+
+		$error = Controller::getError();
+		if ($error !== null) {
+			if (!str_contains($path, "?")) {
+				$path .= "?";
+			}
+
+			$path .= "error=$error";
 		}
 
 		header("location: $path");
