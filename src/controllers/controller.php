9d947d87fd52a47a288d02065569c05f75ceead3
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-11-30 16:54:27 +0330

    Add(controller): get request method for controller base class.

--- src/controllers/controller.php
+++ src/controllers/controller.php
@@ -2,6 +2,14 @@
 
 include_once __DIR__ . "/../config.php";
 
-class Controller {}
+class Controller {
+	final public static function getRequest(string $name, array $req = $_GET): string|null {
+		if (isset($req[$name])) {
+			return $req[$name];
+		}
+
+		return null;
+	}
+}
 
 ?>
