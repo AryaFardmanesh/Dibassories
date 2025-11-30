2d0bdc13302d6cf23e6cfc2e84df8f37fb74b78e
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-11-30 17:02:14 +0330

    Fix(controller): sanitize the input value.

--- src/controllers/controller.php
+++ src/controllers/controller.php
@@ -1,11 +1,12 @@
 <?php
 
 include_once __DIR__ . "/../config.php";
+include_once __DIR__ . "/../utils/sanitizer.php";
 
 class Controller {
 	final public static function getRequest(string $name): string|null {
 		if (isset($_GET[$name])) {
-			return $_GET[$name];
+			return testInput($_GET[$name]);
 		}
 
 		return null;
