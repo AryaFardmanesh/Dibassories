1f205cf02a08edcd34521b02f5c2279be5a891ca
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-12-01 12:34:35 +0330

    Add(controller): update product.

--- src/controllers/products.php
+++ src/controllers/products.php
@@ -26,7 +26,7 @@ if ($req === CONTROLLER_PRODUCT_ADD) {
 		goto out;
 	}
 
-	$type = Controller::getRequest("type", true);
+	$type = (int)Controller::getRequest("type", true);
 	$name = Controller::getRequest("name", true);
 	$description = Controller::getRequest("description", true);
 	$count = (int)Controller::getRequest("count", true);
@@ -56,7 +56,72 @@ if ($req === CONTROLLER_PRODUCT_ADD) {
 		goto out;
 	}
 }elseif ($req === CONTROLLER_PRODUCT_UPDATE) {
-	// ...
+	$product = ProductRepository::findById($productId);
+
+	if (ProductRepository::hasError()) {
+		Controller::setError(ProductRepository::getError());
+		goto out;
+	}
+
+	if ($product === null) {
+		Controller::setError("محصول یافت نشد.");
+		goto out;
+	}
+
+	$type = (int)Controller::getRequest("type");
+	$name = Controller::getRequest("name");
+	$description = Controller::getRequest("description");
+	$price = (int)Controller::getRequest("price");
+	$count = (int)Controller::getRequest("count");
+	$offer = (int)Controller::getRequest("offer");
+	$images = Controller::fetchSerialized("image", 4);
+	$colors = Controller::fetchSerialized("color", CONTROLLER_PRODUCT_LIMIT_COLOR_COUNT, true, ",");
+	$materials = Controller::fetchSerialized("material", CONTROLLER_PRODUCT_LIMIT_MATERIAL_COUNT);
+	$sizes = Controller::fetchSerialized("material", CONTROLLER_PRODUCT_LIMIT_SIZE_COUNT);
+
+	if ($type !== null) $product->type = $type;
+	if ($name) $product->name = $name;
+	if ($description) $product->description = $description;
+	if ($price !== null) $product->price = $price;
+	if ($count !== null) $product->count = $count;
+	if ($offer !== null) $product->offer = $offer;
+
+	ProductRepository::update($product);
+
+	if (ProductRepository::hasError()) {
+		Controller::setError(ProductRepository::getError());
+		goto out;
+	}
+
+	foreach ($colors as $color) {
+		$name = $color[0];
+		$hex = $color[1];
+
+		ProductRepository::updateColor($productId, $name, $hex);
+
+		if (ProductRepository::hasError()) {
+			Controller::setError(ProductRepository::getError());
+			goto out;
+		}
+	}
+
+	foreach ($materials as $material) {
+		ProductRepository::updateMaterial($productId, $material);
+
+		if (ProductRepository::hasError()) {
+			Controller::setError(ProductRepository::getError());
+			goto out;
+		}
+	}
+
+	foreach ($sizes as $size) {
+		ProductRepository::updateSize($productId, $size);
+
+		if (ProductRepository::hasError()) {
+			Controller::setError(ProductRepository::getError());
+			goto out;
+		}
+	}
 }elseif ($req === CONTROLLER_PRODUCT_SUSPEND) {
 	// ...
 }elseif ($req === CONTROLLER_PRODUCT_REMOVE) {
