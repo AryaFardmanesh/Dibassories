4c96ebb048600c2d13af86d4e64099f9f8ff7c29
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-12-04 15:44:59 +0330

    Add(controller): add and remove handlers.

--- src/controllers/carts.php
+++ src/controllers/carts.php
@@ -20,9 +20,27 @@ if ($account === null) {
 }
 
 if ($req === CONTROLLER_CART_ADD_CART) {
-	// ...
+	$productId = Controller::getRequest("product", true);
+	$colorId = Controller::getRequest("color", true);
+	$materialId = Controller::getRequest("material", true);
+	$sizeId = Controller::getRequest("size", true);
+	$count = (int)Controller::getRequest("count", true);
+
+	ShoppingCartRepository::add($user, $productId, $colorId, $sizeId, $materialId, $count);
+
+	if (ShoppingCartRepository::hasError()) {
+		Controller::setError(ShoppingCartRepository::getError());
+		goto out;
+	}
 }elseif ($req === CONTROLLER_CART_REMOVE_CART) {
-	// ....
+	$productId = Controller::getRequest("product", true);
+
+	ShoppingCartRepository::remove($user, $productId);
+
+	if (ShoppingCartRepository::hasError()) {
+		Controller::setError(ShoppingCartRepository::getError());
+		goto out;
+	}
 }elseif ($req === CONTROLLER_CART_EMPTY_CART) {
 	// ....
 }elseif ($req === CONTROLLER_CART_INC_PRODUCT_COUNT) {
