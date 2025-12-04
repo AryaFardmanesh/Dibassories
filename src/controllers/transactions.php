a3d1a527a96b9d0962ae4e3ddbb8314c84fea154
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-12-04 16:17:24 +0330

    Fix(controller): permission check for transaction open handler.

--- src/controllers/transactions.php
+++ src/controllers/transactions.php
@@ -72,6 +72,17 @@ if ($req === CONTROLLER_TRANSACTION_CHARGE) {
 	}
 }elseif ($req === CONTROLLER_TRANSACTION_OPEN) {
 	$transactionId = Controller::getRequest("transaction", true);
+	$transaction = TransactionRepository::find($transactionId);
+
+	if (TransactionRepository::hasError()) {
+		Controller::setError(TransactionRepository::getError());
+		goto out;
+	}
+
+	if ($transaction->wallet !== $owner && $account->role !== ROLE_ADMIN) {
+		Controller::setError("شما مجوز ایجاد تغییر در این تراکنش را ندارید.");
+		goto out;
+	}
 
 	TransactionRepository::updateStatus($transactionId, STATUS_OPENED);
 
