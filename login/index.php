197bbec98f49d17ae1de752c598c180459ae0b19
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-12-06 10:42:39 +0330

    Add(backend): login page.

--- login/index.php
+++ login/index.php
@@ -1,4 +1,34 @@
-<?php include_once __DIR__ . "/../src/config.php"; ?>
+<?php
+
+include_once __DIR__ . "/../src/config.php";
+include_once __DIR__ . "/../src/services/accounts.php";
+include_once __DIR__ . "/../src/controllers/controller.php";
+
+if (AccountService::isLogin()) {
+	Controller::redirect(null);
+}
+
+$error = Controller::fetchError();
+
+Controller::onSubmit("POST", function (array $params) {
+	$result = AccountService::login(
+		$params["username"],
+		$params["password"]
+	);
+
+	if (!$result || AccountService::hasError()) {
+		Controller::setError(AccountService::getError());
+		Controller::redirect(htmlspecialchars($_SERVER["PHP_SELF"]));
+	}
+
+	Controller::redirect(null);
+
+}, [], [
+	"username",
+	"password"
+]);
+
+?>
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
 <head>
@@ -14,10 +44,14 @@
 </head>
 <body>
 	<div class="login-card">
+		<?php
+			if ($error) echo "<div class='alert alert-danger' role='alert'>$error</div>";
+		?>
+
 		<div class="logo">دیبا اکسسوری</div>
 		<h5 class="mb-4 fw-bold fs-6 text-secondary">ورود به حساب کاربری</h5>
 
-		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+		<form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
 			<div class="mb-3 text-start">
 				<label for="username" class="form-label">نام کاربری:</label>
 				<input
