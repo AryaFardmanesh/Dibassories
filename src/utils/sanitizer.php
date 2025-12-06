0ff4f26bd4ffe11ca13aeed275721a9ba9415673
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-12-06 10:32:45 +0330

    Add(util): convert persian number to english.

--- src/utils/sanitizer.php
+++ src/utils/sanitizer.php
@@ -18,4 +18,21 @@ function dbFlatData(string|null $data): string {
 	return "'$data'";
 }
 
+function convertToEnglish(string $raw) {
+	$newNumbers = range(0, 9);
+	// 1. Persian HTML decimal
+	$persianDecimal = array('&#1776;', '&#1777;', '&#1778;', '&#1779;', '&#1780;', '&#1781;', '&#1782;', '&#1783;', '&#1784;', '&#1785;');
+	// 2. Arabic HTML decimal
+	$arabicDecimal = array('&#1632;', '&#1633;', '&#1634;', '&#1635;', '&#1636;', '&#1637;', '&#1638;', '&#1639;', '&#1640;', '&#1641;');
+	// 3. Arabic Numeric
+	$arabic = array('٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩');
+	// 4. Persian Numeric
+	$persian = array('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹');
+
+	$raw =  str_replace($persianDecimal, $newNumbers, $raw);
+	$raw =  str_replace($arabicDecimal, $newNumbers, $raw);
+	$raw =  str_replace($arabic, $newNumbers, $raw);
+	return str_replace($persian, $newNumbers, $raw);
+}
+
 ?>
