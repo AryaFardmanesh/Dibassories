ce6c101e5ca2de09c22e1d5d2e8f9cad1f07e285
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-12-13 16:25:22 +0330

    Fix(database): product description type.

--- database/structure.sql
+++ database/structure.sql
@@ -111,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `dibas_products` (
   `owner` varchar(32) NOT NULL,
   `type` tinyint(4) NOT NULL,
   `name` varchar(128) NOT NULL,
-  `description` text NOT NULL,
+  `description` varchar(512) NOT NULL,
   `image` varchar(512) NOT NULL,
   `count` int(11) NOT NULL,
   `price` decimal(12,2) NOT NULL,
